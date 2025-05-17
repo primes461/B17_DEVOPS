de84d4f858d6f381fbcb0c484870964a41cb7d43
unknown <primes461@gmail.com>
2025-05-17 07:26:45 +0530

    remove code

--- test/main.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  count    = length(var.rg_name)
-  name     = var.rg_name[count.index]                
-  location = "central india"
-}
--- test/variable.tf
+++
@@ -1,6 +0,0 @@
-variable "rg_name" {
-  description = "The name of the resource group"
-  type        = list(string)
-  default     = ["rg-sam-1", "rg-sam-2", "rg-sam-3", "rg-sam-4",]
-              #     0             1             2             3
-}
